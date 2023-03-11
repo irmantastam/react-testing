7a85ea6f962e44d519f3763c72c4169374b29d10
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2023-03-11 10:41:48 +0200

    Reorganise greet test to describe blocks

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -6,14 +6,18 @@
 import { render, screen } from '@testing-library/react';
 import { Greet } from './greet';
 
-test('Greet renders correctly', () => {
-  render(<Greet />);
-  const textElement = screen.getByText('Hello');
-  expect(textElement).toBeInTheDocument();
+describe('Greet', () => {
+  test('Renders correctly', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-test('Greet renders with a name', () => {
-  render(<Greet name="Irmantas"/>);
-  const textElement = screen.getByText('Hello Irmantas');
-  expect(textElement).toBeInTheDocument();
+describe('Nested', () => {
+  test('Renders a name', () => {
+    render(<Greet name="Irmantas"/>);
+    const textElement = screen.getByText('Hello Irmantas');
+    expect(textElement).toBeInTheDocument();
+  });
 });
