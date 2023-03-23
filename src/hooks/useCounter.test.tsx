3ca20dccd0277466211fb6a75b4d1eb6a9703058
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2023-03-23 20:48:25 +0200

    Act Utility

--- src/hooks/useCounter.test.tsx
+++ src/hooks/useCounter.test.tsx
@@ -1,18 +1,30 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useCounter } from './useCounter';
 
 describe('UseCounter', () => {
-  test('shdould render the initial count', () => {
+  test('should render the initial count', () => {
     const { result } = renderHook(useCounter);
     expect(result.current.count).toBe(0);
-  })
+  });
 
-  test('shdould accept and render the same initial count', () => {
+  test('should accept and render the same initial count', () => {
     const { result } = renderHook(useCounter, {
       initialProps: {
         initialCount: 10,
       }
     });
     expect(result.current.count).toBe(10);
-  })
-})
+  });
+
+  test('should increment the count', () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+
+  test('should decrement the count', () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
+  });
+});
