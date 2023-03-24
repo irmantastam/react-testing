6df16da9e709aa1469980ca59dea4dd174933338
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2023-03-24 17:37:36 +0200

    Formatted file

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -72,11 +72,6 @@ describe('Application', () => {
     const submitButtonElement = screen.getByRole('button')
     expect(submitButtonElement).toBeInTheDocument()
 
-
-
-
-
-    
     expect(submitButtonElement).toBeDisabled()
   })
 })
