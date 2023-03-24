711fc5c6bae7281c5e10491afbe308fb966ec352
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2023-03-24 09:57:26 +0200

    Mocking HTTP Requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import { AppProviders } from './providers/app-providers';
-import { MuiMode } from './components/mui/mui-mode';
+import { Users } from './components/users/users';
 
 function App() {
   return (
     <AppProviders>
       <div className="App">
-        <MuiMode />
+        <Users />
       </div>
     </AppProviders>
   );
